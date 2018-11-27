6a4d92c9ab2ada2962b90ce61d4038d01932979b
_ipal <malkoleyplay@gmail.com>
2018-11-27 19:22:47 +0200

    fixed alghoritm, inproccess: drawing y lines

--- srcs/ft_drawing_bresenham_algorithm.c
+++ srcs/ft_drawing_bresenham_algorithm.c
@@ -13,14 +13,15 @@
 #include "../includes/fdf.h"
 #include "../includes/additional_structs.h"
 
-static void	add_draw_line(t_dp delta, t_dp points,
-					float *increase, t_mlxncolor mnc)
+static void	add_draw_line(t_dp delta, t_dp points, t_mlxncolor mnc)
 {
 	int	x;
 	int	y;
 	int	dir;
-
+	int	increase;
+	
 	dir = ZERO;
+	increase = ZERO;
 	x = points.p1.x;
 	y = points.p1.y;
 	if (delta.p1.y)
@@ -28,41 +29,37 @@ static void	add_draw_line(t_dp delta, t_dp points,
 	while (((delta.p1.x > 0) ? (x <= points.p2.x) : (x >= points.p2.x)))
 	{
 		mlx_pixel_put(mnc.mlx->mlx, mnc.mlx->win, x, y, mnc.color);
-		*increase += (float)delta.p2.y / (float)delta.p2.x;
-		if (*increase >= 1.0f)
+		increase += delta.p2.y;
+		if (increase >= delta.p2.x)
 		{
-			*increase -= 1.0f;
+			increase -= delta.p2.x;
 			y += dir;
 		}
 		(delta.p1.x > 0) ? ++x : --x;
 	}
 }
 
-static void	add_set_line(t_point p1, t_point p2, t_mlxncolor mnc)
+static void	add_set_line(t_point point1, t_point point2, t_mlxncolor mnc)
 {
-	const int	deltax = p1.x - p2.x;
-	const int	deltay = p1.y - p2.y;
+	const int	deltax = point2.x - point1.x;
+	const int	deltay = point2.y - point1.y;
 	const int	absdx = _ABS(deltax);
 	const int	absdy = _ABS(deltay);
-	float		increase;
-
-	increase = ZERO;
+	
 	if (absdx >= absdy)
-		add_draw_line((t_dp){{p1.x = deltax,
-								p1.y = deltay},
-							{p2.x = absdx,
-								p2.y = absdy}},
-					(t_dp){.p1 = p1,
-							.p2 = p2},
-					&increase, mnc);
+		add_draw_line((t_dp){.p1.x = deltax,
+								.p1.y = deltay,
+							.p2.x = absdx,
+								.p2.y = absdy},
+					(t_dp){.p1 = point1,
+							.p2 = point2}, mnc);
 	else
-		add_draw_line((t_dp){{p1.x = deltay,
-								p1.y = deltax},
-							{p2.x = absdy,
-								p2.y = absdx}},
-					(t_dp){.p1 = p2,
-							.p2 = p1},
-					&increase, mnc);
+		add_draw_line((t_dp){.p1.x = deltay,
+								.p1.y = deltax,
+							.p2.x = absdy,
+								.p2.y = absdx},
+					(t_dp){.p1 = point2,
+							.p2 = point1}, mnc);
 }
 
 void		pj_mlx_draw_raw_matrix(t_mlx *mlx, t_matrix **matrix)
@@ -80,13 +77,16 @@ void		pj_mlx_draw_raw_matrix(t_mlx *mlx, t_matrix **matrix)
 		dist_x = ZERO;
 		while (++x < g_matrix_x - 1)
 		{
-			add_set_line((t_point){.x = x * dist_x, y * dist_y},
-						(t_point){.x = (x + 1) * dist_x, y * dist_y},
-						(t_mlxncolor){.mlx = mlx, .color = matrix[y][x].z});
+			add_set_line((t_point){.x = x * dist_x, .y = y * dist_y},
+						(t_point){.x = (x + 1) * DEC, .y = y * dist_y},
+						(t_mlxncolor){.mlx = mlx, .color = matrix[y][x].rgb});
+			// add_set_line((t_point){.x = x * dist_x, .y = y * dist_y},
+			// 			(t_point){.x = x * dist_x, .y = (y + 1) * DEC},
+			// 			(t_mlxncolor){.mlx = mlx, .color = matrix[y][x].rgb});
 			dist_x = DEC;
 		}
 		dist_y = DEC;
 	}
+	mlx_loop(mlx->mlx);
 	mlx_destroy_window(mlx->mlx, mlx->win);
-	// mlx_loop(mlx->mlx);
 }
