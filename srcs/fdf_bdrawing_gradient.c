289e17616eb9a5b7393edb0995f1d64f1effa040
Iipal <malkoleyplay@gmail.com>
2018-12-25 12:35:05 +0200

    improve gradient

--- srcs/fdf_bdrawing_gradient.c
+++ srcs/fdf_bdrawing_gradient.c
@@ -6,7 +6,7 @@
 /*   By: ipal <ipal@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 20:12:15 by tmaluh            #+#    #+#             */
-/*   Updated: 2018/12/23 18:57:55 by ipal             ###   ########.fr       */
+/*   Updated: 2018/12/25 12:23:26 by ipal             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,28 +25,28 @@ int	fdf_glen(int delta, int point, int xy)
 	return (out_len);
 }
 
-int	*fdf_gradient(t_g *gradient, int gradient_len)
+int	*fdf_gradient(t_g *g, int glen)
 {
 	t_rgb	diff;
 	t_rgb	start;
-	t_rgb	end;
 	int		*out_rgb;
 	int		i;
 
 	i = NEG;
-	start = (t_rgb){gradient->start >> 16, (gradient->start >> 8) & 0xff,
-					gradient->start & 0xff};
-	end = (t_rgb){gradient->end >> 16, (gradient->end >> 8) & 0xff,
-				gradient->end & 0xff};
-	diff = (t_rgb){end.r - start.r,
-				end.g - start.g,
-				end.b - start.b};
-	out_rgb = (int*)malloc(sizeof(int) * gradient_len);
-	while (++i < gradient_len)
+	out_rgb = (int*)malloc(sizeof(int) * glen);
+	if (g->start == g->end)
 	{
-		start = (t_rgb){start.r + diff.r / gradient_len,
-						start.g + diff.g / gradient_len,
-						start.b + diff.b / gradient_len};
+		while (++i < glen)
+			out_rgb[i] = g->start;
+		return (out_rgb);
+	}
+	start = (t_rgb){g->start >> 16, (g->start >> 8) & 0xff, g->start & 0xff};
+	diff = (t_rgb){(g->end >> 16) - start.r, ((g->end >> 8) & 0xff) - start.g,
+					(g->end & 0xff) - start.b};
+	while (++i < glen)
+	{
+		start = (t_rgb){start.r + diff.r / glen, start.g + diff.g / glen,
+						start.b + diff.b / glen};
 		out_rgb[i] = ((int)start.r << 16) | ((int)start.g << 8) | ((int)start.b);
 	}
 	return (out_rgb);
